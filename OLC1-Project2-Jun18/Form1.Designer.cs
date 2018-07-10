b46f3e95f83fb4ef0181e13edbab042faba1230b
MacoChave <totto_cha@hotmail.com>
2018-07-10 00:29:51 -0600

    GUI y gramatica en txt

--- OLC1-Project2-Jun18/Form1.Designer.cs
+++ OLC1-Project2-Jun18/Form1.Designer.cs
@@ -28,12 +28,250 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.guardarMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarTodoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.cerrarMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarTodoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compilarMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compilarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.arbolMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.astMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sintacticoAbstractoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.reformatearMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator6 = new System.Windows.Forms.ToolStripSeparator();
+            this.ejecutarMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoMenuItem,
+            this.compilarMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoMenuItem
+            // 
+            this.archivoMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirMenuItem,
+            this.nuevoMenuItem,
+            this.toolStripSeparator1,
+            this.guardarMenuItem,
+            this.guardarTodoMenuItem,
+            this.toolStripSeparator2,
+            this.cerrarMenuItem,
+            this.cerrarTodoMenuItem,
+            this.toolStripSeparator3,
+            this.salirMenuItem});
+            this.archivoMenuItem.Name = "archivoMenuItem";
+            this.archivoMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoMenuItem.Text = "Archivo";
+            // 
+            // abrirMenuItem
+            // 
+            this.abrirMenuItem.Name = "abrirMenuItem";
+            this.abrirMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.abrirMenuItem.Text = "Abrir";
+            this.abrirMenuItem.Click += new System.EventHandler(this.abrirMenuItem_Click);
+            // 
+            // nuevoMenuItem
+            // 
+            this.nuevoMenuItem.Name = "nuevoMenuItem";
+            this.nuevoMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.nuevoMenuItem.Text = "Nuevo";
+            this.nuevoMenuItem.Click += new System.EventHandler(this.nuevoMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(143, 6);
+            // 
+            // guardarMenuItem
+            // 
+            this.guardarMenuItem.Name = "guardarMenuItem";
+            this.guardarMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.guardarMenuItem.Text = "Guardar";
+            this.guardarMenuItem.Click += new System.EventHandler(this.guardarMenuItem_Click);
+            // 
+            // guardarTodoMenuItem
+            // 
+            this.guardarTodoMenuItem.Name = "guardarTodoMenuItem";
+            this.guardarTodoMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.guardarTodoMenuItem.Text = "Guardar Todo";
+            this.guardarTodoMenuItem.Click += new System.EventHandler(this.guardarTodoMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(143, 6);
+            // 
+            // cerrarMenuItem
+            // 
+            this.cerrarMenuItem.Name = "cerrarMenuItem";
+            this.cerrarMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.cerrarMenuItem.Text = "Cerrar";
+            this.cerrarMenuItem.Click += new System.EventHandler(this.cerrarMenuItem_Click);
+            // 
+            // cerrarTodoMenuItem
+            // 
+            this.cerrarTodoMenuItem.Name = "cerrarTodoMenuItem";
+            this.cerrarTodoMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.cerrarTodoMenuItem.Text = "Cerrar Todo";
+            this.cerrarTodoMenuItem.Click += new System.EventHandler(this.cerrarTodoMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(143, 6);
+            // 
+            // salirMenuItem
+            // 
+            this.salirMenuItem.Name = "salirMenuItem";
+            this.salirMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.salirMenuItem.Text = "Salir";
+            this.salirMenuItem.Click += new System.EventHandler(this.salirMenuItem_Click);
+            // 
+            // compilarMenuItem
+            // 
+            this.compilarMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.compilarToolStripMenuItem1,
+            this.toolStripSeparator4,
+            this.arbolMenuItem,
+            this.toolStripSeparator5,
+            this.reformatearMenuItem,
+            this.toolStripSeparator6,
+            this.ejecutarMenuItem});
+            this.compilarMenuItem.Name = "compilarMenuItem";
+            this.compilarMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.compilarMenuItem.Text = "Analisis";
+            // 
+            // compilarToolStripMenuItem1
+            // 
+            this.compilarToolStripMenuItem1.Name = "compilarToolStripMenuItem1";
+            this.compilarToolStripMenuItem1.Size = new System.Drawing.Size(181, 22);
+            this.compilarToolStripMenuItem1.Text = "Compilar";
+            this.compilarToolStripMenuItem1.Click += new System.EventHandler(this.compilarToolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(178, 6);
+            // 
+            // arbolMenuItem
+            // 
+            this.arbolMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.astMenuItem,
+            this.sintacticoAbstractoMenuItem});
+            this.arbolMenuItem.Name = "arbolMenuItem";
+            this.arbolMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.arbolMenuItem.Text = "Arbol";
+            // 
+            // astMenuItem
+            // 
+            this.astMenuItem.Name = "astMenuItem";
+            this.astMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.astMenuItem.Text = "AST";
+            this.astMenuItem.Click += new System.EventHandler(this.astMenuItem_Click);
+            // 
+            // sintacticoAbstractoMenuItem
+            // 
+            this.sintacticoAbstractoMenuItem.Name = "sintacticoAbstractoMenuItem";
+            this.sintacticoAbstractoMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sintacticoAbstractoMenuItem.Text = "Sintactico Abstracto";
+            this.sintacticoAbstractoMenuItem.Click += new System.EventHandler(this.sintacticoAbstractoMenuItem_Click);
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(178, 6);
+            // 
+            // reformatearMenuItem
+            // 
+            this.reformatearMenuItem.Name = "reformatearMenuItem";
+            this.reformatearMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.reformatearMenuItem.Text = "Reformatear Codigo";
+            this.reformatearMenuItem.Click += new System.EventHandler(this.reformatearMenuItem_Click);
+            // 
+            // toolStripSeparator6
+            // 
+            this.toolStripSeparator6.Name = "toolStripSeparator6";
+            this.toolStripSeparator6.Size = new System.Drawing.Size(178, 6);
+            // 
+            // ejecutarMenuItem
+            // 
+            this.ejecutarMenuItem.Name = "ejecutarMenuItem";
+            this.ejecutarMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.ejecutarMenuItem.Text = "Ejecutar";
+            this.ejecutarMenuItem.Click += new System.EventHandler(this.ejecutarMenuItem_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Location = new System.Drawing.Point(0, 24);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(784, 437);
+            this.tabControl.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.ToolStripMenuItem archivoMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarTodoMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarTodoMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compilarMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arbolMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem astMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sintacticoAbstractoMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem compilarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripMenuItem reformatearMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator6;
+        private System.Windows.Forms.ToolStripMenuItem ejecutarMenuItem;
     }
 }
 
--- OLC1-Project2-Jun18/Form1.cs
+++ OLC1-Project2-Jun18/Form1.cs
@@ -1,20 +1,265 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace OLC1_Project2_Jun18
 {
     public partial class Form1 : Form
     {
+        private static string PAGE_NAME = "page";
+        private int COUNTER = 0;
+        private static string FILENAME_DEFAULT = "Nuevo";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /*******************************************************
+         * ****************************************************
+         * CONTROL DE AREA DE CÓDIGO
+         * ****************************************************
+         ******************************************************/
+        private void createSourceArea(TabPage page, string text)
+        {
+            RichTextBox rowCount = new RichTextBox()
+            {
+                Name = "row" + COUNTER,
+                Dock = DockStyle.Left,
+                Width = (page.Width * 10) / 100,
+                Parent = page,
+                ForeColor = Color.MediumPurple,
+                Font = new Font("Consolas", 12, FontStyle.Regular),
+                WordWrap = false,
+            };
+
+            RichTextBox box = new RichTextBox()
+            {
+                Name = "box" + COUNTER,
+                Text = text,
+                Dock = DockStyle.Right,
+                Width = (page.Width * 90) / 100,
+                Parent = page,
+                AcceptsTab = true,
+                ForeColor = Color.MidnightBlue,
+                Font = new Font("Consolas", 12, FontStyle.Regular),
+                WordWrap = true
+            };
+
+            box.TextChanged += Box_TextChanged;
+            box.KeyUp += Box_KeyUp;
+            box.Click += Box_Click;
+            box.VScroll += Box_VScroll;
+
+            page.Controls.Add(rowCount);
+            page.Controls.Add(box);
+        }
+
+        private void createTabPage(string path, string filename, string text)
+        {
+            TabPage page = new TabPage()
+            {
+                Name = PAGE_NAME + COUNTER,
+                Text = filename,
+                Tag = path
+            };
+
+            tabControl.TabPages.Add(page);
+            createSourceArea(page, text);
+            tabControl.SelectedTab = page;
+            COUNTER++;
+        }
+
+        private void Box_VScroll(object sender, EventArgs e)
+        {
+            RichTextBox box = (RichTextBox)sender;
+            Control parent = box.Parent;
+            RichTextBox row = (RichTextBox)parent.Controls[0];
+        }
+
+        private void Box_Click(object sender, EventArgs e) 
+            => getPositionCursor((RichTextBox)sender);
+
+        private void Box_KeyUp(object sender, KeyEventArgs e) 
+            => getPositionCursor((RichTextBox)sender);
+
+        private void Box_TextChanged(object sender, EventArgs e)
+        {
+            getPositionCursor((RichTextBox)sender);
+        }
+
+        private void getPositionCursor(RichTextBox box)
+        {
+            int selection_start, first_index, current_line;
+            selection_start = box.SelectionStart;
+            current_line = box.GetLineFromCharIndex(selection_start);
+            first_index = box.GetFirstCharIndexOfCurrentLine();
+            int x = selection_start - first_index;
+
+            Console.WriteLine($"x: {x}, y: {current_line}");
+        }
+
+        /*******************************************************
+         * ****************************************************
+         * ABRIR Y CREAR ARCHIVO
+         * ****************************************************
+         ******************************************************/
+        private void abrirMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Title = "Seleccionar archivo",
+                Filter = "Source CRL | *.crl",
+                CheckFileExists = true,
+                CheckPathExists = true
+            };
+            ofd.ShowDialog();
+
+            try
+            {
+                StreamReader sr = new StreamReader(ofd.FileName);
+                createTabPage(ofd.FileName, ofd.SafeFileName, sr.ReadToEnd());
+                sr.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void nuevoMenuItem_Click(object sender, EventArgs e) 
+            => createTabPage("", FILENAME_DEFAULT, "");
+
+        /*******************************************************
+         * ****************************************************
+         * GUARDAR ARCHIVO
+         * ****************************************************
+         ******************************************************/
+        private void guardarMenuItem_Click(object sender, EventArgs e)
+        {
+            TabPage currentPage = tabControl.SelectedTab;
+            RichTextBox box = (RichTextBox)currentPage.Controls[0];
+            string path = currentPage.Tag.ToString();
+
+            if (path.Length < 1)
+            {
+                // GUARDAR COMO
+                saveAs(box, currentPage);
+            }
+            else
+            {
+                // GUARDAR
+                try
+                {
+                    StreamWriter write = new StreamWriter(path);
+                    write.Write(box.Text);
+                    write.Close();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void guardarTodoMenuItem_Click(object sender, EventArgs e)
+        {
+            TabPage currentPage = tabControl.SelectedTab;
+            RichTextBox box = (RichTextBox)currentPage.Controls[0];
+            // GUARDAR COMO
+            saveAs(box, currentPage);
+        }
+
+        private void saveAs(RichTextBox box, TabPage currentPage)
+        {
+            SaveFileDialog sfd = new SaveFileDialog()
+            {
+                Title = "Seleccionar destino",
+                Filter = "Source CRL | *.crl"
+            };
+            sfd.ShowDialog();
+
+            try
+            {
+                StreamWriter writer = new StreamWriter(sfd.FileName);
+                writer.Write(box.Text);
+
+                currentPage.Tag = sfd.FileName;
+                currentPage.Text = sfd.FileName;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        
+        /*******************************************************
+        * ****************************************************
+        * CERRAR UNA O VARIOS ARCHIVOS
+        * ****************************************************
+        ******************************************************/
+        private void cerrarMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tabControl.TabPages.Remove(tabControl.SelectedTab);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void cerrarTodoMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tabControl.TabPages.Clear();
+                COUNTER = 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void salirMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /*******************************************************
+         * ****************************************************
+         * COMPILAR
+         * GENERAR ARBOLES AST Y SINTACTICO ABSTRACTO
+         * REFACTORIZAR CODIGO
+         * EJECUTAR CODIGO
+         * ****************************************************
+         ******************************************************/
+        private void compilarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void astMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sintacticoAbstractoMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reformatearMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ejecutarMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
